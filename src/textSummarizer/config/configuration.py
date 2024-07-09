95f0b6a699232dea815b6040b81c6b1e4e8e15ee
RisAhamed <riswanahamed38@gmail.com>
2024-07-09 21:30:46 +0530

    Data Transformation Added

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from src.textSummarizer.constants import PARAMS_FILE_PATH,CONFIG_FILE_PATH
 from src.textSummarizer.utils.common import read_yaml,create_directories
 from pathlib import Path
-from src.textSummarizer.entity import DataIngestionConfig,DataValidationConfig
+from src.textSummarizer.entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(self, params_filepath=PARAMS_FILE_PATH, config_filepath=CONFIG_FILE_PATH):
@@ -34,3 +34,13 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config['data_transformation']
+        create_directories([Path(config['root_dir'])])
+        data_transformation_config = DataTransformationConfig(
+            root_dir=Path(config['root_dir']),
+            data_path=Path(config['data_path']),
+            tokenizer_name=config['tokenizer_name']  # This remains a string
+        )
+        return data_transformation_config
